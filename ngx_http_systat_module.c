fcd4dfebd621d76243ad0d7aa12f427cfe92e5a0
Eugene Mychlo <myc@barev.net>
2015-08-07 18:27:01 +0300

    Initial commit

--- ngx_http_systat_module.c
+++ ngx_http_systat_module.c
@@ -1,67 +1,70 @@
+/*
+ * Copyright (c) 2015 Eugene Mychlo
+ */
+
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
 #include <nginx.h>
 
+#include <ifaddrs.h>
 
-#define NGX_HTTP_SYSTAT_FORMAT_PLAIN    0
-#define NGX_HTTP_SYSTAT_FORMAT_XML      1
 
-#define NGX_HTTP_SYSTAT_IFS_TXBYTES     0x0001
-#define NGX_HTTP_SYSTAT_IFS_RXBYTES     0x0002
+#if (NGX_LINUX)
+#include <linux/netdevice.h>
+
+#define NGX_AF_LINK     AF_PACKET
+#define ngx_get_ifa_tx_bytes(ifa) \
+    ((uint64_t)((struct net_device_stats *)ifa->ifa_data)->tx_bytes)
+
+#elif (NGX_FREEBSD) 
+
+#define NGX_AF_LINK     AF_LINK
+#define ngx_get_ifa_tx_bytes(ifa) \
+    ((uint64_t)((struct if_data *)ifa->ifa_data)->ifi_obytes)
+
+#elif (NGX_DARWIN)
+
+#include <net/if.h>
+#define NGX_AF_LINK     AF_LINK
+#define ngx_get_ifa_tx_bytes(ifa) \
+    ((uint64_t)((struct if_data *)ifa->ifa_data)->ifi_obytes)
+
+#endif
+
+
+
+
+
+#define ngx_getifaddrs(ifa) getifaddrs(ifa)
+#define ngx_getifaddrs_n    "getifaddrs()"
+
+#define ngx_freeifaddrs(ifa) freeifaddrs(ifa)
+
 
 
 typedef struct {
-    ngx_uint_t  param;
-    ngx_uint_t  format;
+    ngx_str_t  netif;
 } ngx_http_systat_loc_conf_t;
 
 
 
-static char *ngx_http_systat_set(ngx_conf_t *cf, ngx_command_t *cmd,
-    void *conf);
-
 static void *ngx_http_systat_create_loc_conf(ngx_conf_t *cf);
 static char *ngx_http_systat_merge_loc_conf(ngx_conf_t *cf,
     void *parent, void *child);
-
-
-
-static ngx_conf_enum_t  ngx_http_systat_format[] = {
-  { ngx_string("plain"), NGX_HTTP_SYSTAT_FORMAT_PLAIN },
-  { ngx_string("xml"),   NGX_HTTP_SYSTAT_FORMAT_XML   },
-  { ngx_null_string, 0 }
-};
-
-static ngx_conf_bitmask_t  ngx_http_systat_param_mask[] = {
-  { ngx_string("ifstat"), NGX_HTTP_SYSTAT_IFSTAT },
-  { ngx_null_string, 0 }
-};
-
-
+static char *ngx_http_systat_set(ngx_conf_t *cf, ngx_command_t *cmd,
+    void *conf);
+static ngx_int_t ngx_http_systat_handler(ngx_http_request_t *r);
+static ngx_int_t ngx_http_systat_netif_tx_bytes(ngx_http_request_t *r,
+    uint64_t *counter);
 
 static ngx_command_t ngx_http_systat_commands[] = {
   { ngx_string("systat"),
-    NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
+    NGX_HTTP_LOC_CONF|NGX_CONF_TAKE2,
     ngx_http_systat_set,
     NGX_HTTP_LOC_CONF_OFFSET,
     0,
     NULL },
-
-  { ngx_string("systat_format"),
-    NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_1MORE,
-    ngx_conf_set_enum_slot,
-    NGX_HTTP_LOC_CONF_OFFSET,
-    offsetof(ngx_http_systat_loc_conf_t, format),
-    &ngx_http_systat_format },
-
-
-  { ngx_string("systat_param"),
-    NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_1MORE,
-    ngx_conf_set_bitmask_slot,
-    NGX_HTTP_LOC_CONF_OFFSET,
-    offsetof(ngx_http_systat_loc_conf_t, param),
-    &ngx_http_systat_param_mask },
 
     ngx_null_command
 };
@@ -100,12 +103,10 @@ ngx_module_t ngx_http_systat_module = {
 static ngx_int_t
 ngx_http_systat_handler(ngx_http_request_t *r)
 {
-    ngx_int_t                       rc;
-    ngx_chain_t                     out;
-    ngx_buf_t                      *b;
-    //ngx_uint_t                      format;
-    ngx_http_systat_loc_conf_t     *stcf;
-
+    ngx_int_t           rc;
+    ngx_chain_t         out;
+    ngx_buf_t           *b;
+    uint64_t            txb;
 
     if (r->method != NGX_HTTP_GET && r->method != NGX_HTTP_HEAD) {
         return NGX_HTTP_NOT_ALLOWED;
@@ -116,34 +117,22 @@ ngx_http_systat_handler(ngx_http_request_t *r)
         return rc;
     }
 
-    stcf = ngx_http_get_module_loc_conf(r, ngx_http_systat_module);
-
-    switch (stcf->format) {
-
-        case NGX_HTTP_SYSTAT_FORMAT_XML: 
-            ngx_str_set(&r->headers_out.content_type, "text/xml");
-            ngx_str_set(&r->headers_out.charset, "utf-8");
-            break;
-
-        default: /* NGX_HTTP_SYSTAT_FORMAT_PLAIN */
-            ngx_str_set(&r->headers_out.content_type, "text/plain");
-            break;
-    }
-
+    ngx_str_set(&r->headers_out.content_type, "text/plain");
     r->headers_out.content_type_len = r->headers_out.content_type.len;
     r->headers_out.content_type_lowcase = NULL;
+    r->headers_out.status = NGX_HTTP_OK;
 
-    if (r->method == NGX_HTTP_HEAD) {
-        r->headers_out.status = NGX_HTTP_OK;
-
-        rc = ngx_http_send_header(r);
-
-        if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+    rc = ngx_http_send_header(r);
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
             return rc;
-        }
     }
 
-    b = ngx_create_temp_buf(r->pool, 2);
+    rc = ngx_http_systat_netif_tx_bytes(r, &txb);
+    if (rc != NGX_OK) {
+            return rc;
+    }
+    
+    b = ngx_create_temp_buf(r->pool, NGX_INT64_LEN);
     if (b == NULL) {
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
     }
@@ -151,7 +140,7 @@ ngx_http_systat_handler(ngx_http_request_t *r)
     out.buf = b;
     out.next = NULL;
 
-    b->last = ngx_sprintf(b->last, "OK");
+    b->last = ngx_sprintf(b->last, "%uL", txb);
 
     r->headers_out.status = NGX_HTTP_OK;
     r->headers_out.content_length_n = b->last - b->pos;
@@ -162,13 +151,42 @@ ngx_http_systat_handler(ngx_http_request_t *r)
 
     b->last_in_chain = 1;
 
-    rc = ngx_http_send_header(r);
+    return ngx_http_output_filter(r, &out);
+}
 
-    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
-        return rc;
+
+static ngx_int_t
+ngx_http_systat_netif_tx_bytes(ngx_http_request_t *r, uint64_t *counter)
+{
+    ngx_http_systat_loc_conf_t     *stcf;
+    struct ifaddrs *ifaddr, *ifa;
+
+    stcf = ngx_http_get_module_loc_conf(r, ngx_http_systat_module);
+    if (!stcf) {
+        return NGX_ERROR;
     }
 
-    return ngx_http_output_filter(r, &out);
+    
+    if (ngx_getifaddrs(&ifaddr) == NGX_ERROR) {
+        ngx_log_error(NGX_LOG_CRIT, r->connection->log, ngx_errno,
+                      ngx_getifaddrs_n " failed");
+        return NGX_ERROR;
+    }
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL || ifa->ifa_addr->sa_family != NGX_AF_LINK) {
+            continue;
+        }
+
+        if (ngx_strcmp(ifa->ifa_name, stcf->netif.data) == 0) {
+            *counter = ngx_get_ifa_tx_bytes(ifa);
+            ngx_freeifaddrs(ifaddr);
+            return NGX_OK;
+        }
+    }
+
+    ngx_freeifaddrs(ifaddr);
+    return NGX_HTTP_NOT_FOUND;
 }
 
 
@@ -185,11 +203,8 @@ ngx_http_systat_create_loc_conf(ngx_conf_t *cf)
     /*
      * set by ngx_pcalloc():
      *
-     *     conf->param = 0;
+     *     conf->netif = { 0, NULL };
      */
-
-    conf->format = NGX_CONF_UNSET_UINT;
-
 
     return conf;
 }
@@ -200,11 +215,8 @@ ngx_http_systat_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
     ngx_http_systat_loc_conf_t  *prev = parent;
     ngx_http_systat_loc_conf_t  *conf = child;
 
-    ngx_conf_merge_bitmask_value(conf->param, prev->param,
-                                 NGX_CONF_BITMASK_SET);
+    ngx_conf_merge_str_value(conf->netif, prev->netif, "");
 
-    ngx_conf_merge_uint_value(conf->format, prev->format,
-                              NGX_HTTP_SYSTAT_FORMAT_PLAIN);
 
     return NGX_CONF_OK;
 }
@@ -213,7 +225,19 @@ ngx_http_systat_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 static char *
 ngx_http_systat_set(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
+    ngx_http_systat_loc_conf_t *stcf = conf;
     ngx_http_core_loc_conf_t   *clcf;
+    ngx_str_t  *value = cf->args->elts;
+
+    if (ngx_strcmp(value[1].data, "netif_tx_bytes") != 0) {
+        return "take an unsupported argument"; //XXX
+    }
+
+    if (stcf->netif.len) {
+        return "is duplicate";
+    }
+
+    stcf->netif = value[2];
 
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
     clcf->handler = ngx_http_systat_handler;
